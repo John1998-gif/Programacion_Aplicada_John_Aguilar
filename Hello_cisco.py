708cb311762b60b0e2a259b322362b6e3df386c3
John1998-gif <74988117+John1998-gif@users.noreply.github.com>
2021-02-08 22:58:05 -0500

    Add files via upload

---
+++ Hello_cisco.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 24 15:44:44 2020
+
+@author: John
+"""
+
+str1="Cisco"
+str2="Networking"
+str3="Academy"
+space=" "
+print(str1+space+str2+space+str3)
+print(str1+space+str2+space+str3+"\n")
+print(str1+str2+str3)
+print(str1,str2,str3)
+x=5
+x=str(x)
+x=float(x)
+x=bool(x)
+x=int(x)
