22b5e45ad26a94e8c538ffa0cfa7fffea848e620
John1998-gif <74988117+John1998-gif@users.noreply.github.com>
2021-02-08 23:28:36 -0500

    Add files via upload

---
+++ 03_personal_inform.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 22 14:33:36 2020
+
+@author: HOME
+"""
+
+nombre=input("Ingrese su nombre")
+apellido=input("Ingrese su apellido")
+localidad=input("Ingrese su localidad")
+edad=input("Ingrese su edad")
+space=""
+print(nombre,space,apellido,space,localidad,edad)
